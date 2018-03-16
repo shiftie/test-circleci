27c810bcb776f4c03ace046f998ce95f429aa57a
Gordan Dzinovic <gdzinovic@zendesk.com>
2018-03-16 15:05:04 +0100

    fg

--- scripts/create.js
+++ scripts/create.js
@@ -6,9 +6,13 @@ const mkdirp = require('mkdirp');
 const argv = require('yargs').argv;
 const distFolder = `dist`;
 
-if (!!argv.target) {
+if (!argv.target) {
   console.log(glob.sync(`${distFolder}/**/*.txt`));
 } else {
   mkdirp.sync(distFolder);
-  fs.writeFileSync(`${distFolder}/${target}.txt`, `file ${target}`, `utf-8`);
+  fs.writeFileSync(
+    `${distFolder}/${argv.target}.txt`,
+    `file ${argv.target}`,
+    `utf-8`
+  );
 }
