6d0b2214592853401046732feca01c246a220443
Gordan Dzinovic <gdzinovic@zendesk.com>
2018-03-21 18:53:32 +0100

    df

--- scripts/generateTasks.js
+++ scripts/generateTasks.js
@@ -6,9 +6,9 @@ fs.writeFileSync('/tmp/mergedMap.csv', '', 'utf-8');
 fs.writeFileSync(
   '.circleci/build.txt',
   [
-    'node scripts/create.js --target=a $BUILD_PARAMS',
-    'node scripts/create.js --target=b $BUILD_PARAMS',
-    'node scripts/create.js --target=c $BUILD_PARAMS',
+    'node scripts/create.js --target=a $(BUILD_PARAMS)',
+    'node scripts/create.js --target=b $(BUILD_PARAMS)',
+    'node scripts/create.js --target=c $(BUILD_PARAMS)',
   ].join('\n'),
   'utf-8'
 );
