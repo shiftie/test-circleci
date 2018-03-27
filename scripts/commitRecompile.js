68db533588ae486474aa18a0a0c5262e51c630d9
Gordan Dzinovic <gdzinovic@zendesk.com>
2018-03-27 17:51:31 +0200

    daily

---
+++ scripts/commitRecompile.js
@@ -0,0 +1,6 @@
+console.log(`
+Undo some changes (check buildRecompile.js):
+  - Undo changes for always recompiled pages (mapper.CONTENT_MAP)
+  - Undo changes for public/assets/sitemaps/en/ (sitemaps)
+Commit / Push to a unique branch with datetime in its name
+`);
